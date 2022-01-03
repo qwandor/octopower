0b25b536686ae8ae261933349d5cd166be0cbc14
Andrew Walbran <qwandor@google.com>
2022-01-03 17:28:20 +0000

    Specify parameters on command-line for example.

--- examples/readings.rs
+++ examples/readings.rs
@@ -1,17 +1,38 @@
-use eyre::Report;
+use eyre::{bail, Report};
 use octopower::{authenticate, consumption, MeterType};
+use std::process::exit;
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     pretty_env_logger::init();
 
-    let token = authenticate("username", "password").await?;
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 6 {
+        eprintln!("Usage:");
+        eprintln!(
+            "  {} <email address> <password> (electricity|gas) <MPxN> <meter serial>",
+            args[0]
+        );
+        exit(1);
+    }
+    let email_address = &args[1];
+    let password = &args[2];
+    let meter_type = match args[3].as_ref() {
+        "electricity" => MeterType::Electricity,
+        "gas" => MeterType::Gas,
+        t => bail!("Invalid meter type {}", t),
+    };
+    let mpxn = &args[4];
+    let serial = &args[5];
+
+    let token = authenticate(email_address, password).await?;
 
     println!("Auth token: {:?}", token);
 
-    let consumption = consumption(&token, MeterType::Electricity, "MPAN", "serial").await?;
+    let consumption = consumption(&token, meter_type, mpxn, serial).await?;
     println!(
-        "Electricity consumption: {}/{} records",
+        "{:?} consumption: {}/{} records",
+        meter_type,
         consumption.results.len(),
         consumption.count
     );
