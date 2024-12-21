f0ebcf24b3625d2a10efbcb9e5c4ad471ea4d1b8
Andrew Walbran <qwandor@google.com>
2024-12-21 21:59:22 +0000

    Keep polling production stats in example.

--- enphase-local/examples/info.rs
+++ enphase-local/examples/info.rs
@@ -2,10 +2,13 @@
 // This project is dual-licensed under Apache 2.0 and MIT terms.
 // See LICENSE-APACHE and LICENSE-MIT for details.
 
-use enphase_local::Envoy;
+use enphase_local::{
+    production::{Device, DeviceType, MeasurementType},
+    Envoy,
+};
 use eyre::Report;
 use reqwest::Url;
-use std::process::exit;
+use std::{process::exit, thread::sleep, time::Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
@@ -25,5 +28,45 @@ async fn main() -> Result<(), Report> {
     println!("Inventory: {:#?}", envoy.inventory(true).await?);
     println!("Production: {:#?}", envoy.production().await?);
 
-    Ok(())
+    loop {
+        let production = envoy.production().await?;
+        for device in &production.production {
+            print_stats(device);
+        }
+        for device in &production.consumption {
+            print_stats(device);
+        }
+
+        sleep(Duration::from_secs(2));
+    }
+}
+
+fn print_stats(device: &Device) {
+    match device.type_ {
+        DeviceType::Eim => {
+            let measurement_type = match device.measurement_type.unwrap() {
+                MeasurementType::Production => "Producing",
+                MeasurementType::TotalConsumption => "Consuming",
+                MeasurementType::NetConsumption => "Net      ",
+            };
+            let details = device.details.as_ref().unwrap();
+            println!(
+                "{}: {:9} {:7.3} W, {} Wh so far today, {} Wh total",
+                device.reading_time,
+                measurement_type,
+                device.w_now,
+                details.wh_today,
+                details.wh_lifetime,
+            );
+        }
+        DeviceType::Inverters => {
+            println!(
+                "{} inverters producing {} W",
+                device.active_count, device.w_now
+            );
+        }
+        device_type => {
+            println!("Unsupported device type {:?}", device_type);
+        }
+    }
 }
